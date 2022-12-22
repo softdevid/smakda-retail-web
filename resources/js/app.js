e5afadf54b1783a8ea7753193de9ea6543d08000
Dzaky Fadli <zaaaafl654@gmail.com>
2022-12-22 12:24:06 +0700

    re fix flowbite.js & config tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,10 @@ import { render } from 'react-dom';
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'SMAKDA Retail';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} | ${appName}`,
     resolve: (name) => require(`./Pages/${name}`),
     setup({ el, App, props }) {
         return render(<App {...props} />, el);
