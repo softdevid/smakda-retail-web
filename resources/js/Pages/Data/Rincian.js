be6d935219eabc34614faa35d44fa064cef16d8f
zionexeonch <zionexeon@gmail.com>
2022-12-28 15:26:37 +0700

    next commit

--- resources/js/Pages/Data/Rincian.js
+++ resources/js/Pages/Data/Rincian.js
@@ -3,23 +3,23 @@ import Input from "@/Components/Input";
 import { Inertia } from "@inertiajs/inertia";
 import { Link } from "@inertiajs/inertia-react";
 import React, { useEffect, useRef, useState } from "react";
+import * as XLSX from "xlsx";
 
 const Rincian = (props) => {
-
-  const [bulan, setBulan] = useState('');
-  const [tahun, setTahun] = useState('');
+  const [bulan, setBulan] = useState("");
+  const [tahun, setTahun] = useState("");
 
   const handleSubmitBulan = (e) => {
     e.preventDefault();
 
     const data = {
-      bulan, nik: props.dataGuru.nik
-    }
+      bulan,
+      nik: props.dataGuru.nik,
+    };
     console.log(data);
 
     Inertia.get(`/cek-saldo/bulan/${props.dataGuru.nik}/${bulan}`, data);
-
-  }
+  };
 
   const divRef = useRef(null);
   const buttonsRef = useRef([]);
@@ -38,13 +38,13 @@ const Rincian = (props) => {
   //   }
   // }, []);
 
-  const addButton = button => {
+  const addButton = (button) => {
     buttonsRef.current.push(button);
-  }
+  };
 
   const printDiv = () => {
     window.print(divRef.current);
-  }
+  };
 
   // const printContent = document.getElementById('print-content');
   // const printFrame = document.getElementById('print-frame');
@@ -58,7 +58,6 @@ const Rincian = (props) => {
     <>
       {/* <div className="container mx-auto my-4" ref={divRef} id="print-content"> */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-
         <div>
           <div className="container mx-auto my-4" id="print-content">
             <div className="text-center font-bold mx-auto my-4 text-2xl">
@@ -71,19 +70,29 @@ const Rincian = (props) => {
                     <h1 className="px-2 text-lg">NIK : {props.dataGuru.nik}</h1>
                   </div>
                   <div className="flex items-center mt-4 text-gray-700">
-                    <h1 className="px-2 text-lg">Nama : {props.dataGuru.nama}</h1>
+                    <h1 className="px-2 text-lg">
+                      Nama : {props.dataGuru.nama}
+                    </h1>
                   </div>
                   <div className="flex items-center mt-4 text-gray-700">
-                    <p className="px-2 text-lg">Alamat : {props.dataGuru.alamat}</p>
+                    <p className="px-2 text-lg">
+                      Alamat : {props.dataGuru.alamat}
+                    </p>
                   </div>
                   <div className="flex items-center mt-4 text-gray-700">
-                    <h1 className="px-2 text-lg">Jabatan : {props.dataGuru.jabatan}</h1>
+                    <h1 className="px-2 text-lg">
+                      Jabatan : {props.dataGuru.jabatan}
+                    </h1>
                   </div>
                   <div className="flex items-center mt-4 text-gray-700">
-                    <h1 className="px-2 text-lg">Jenis Kelamin : {props.dataGuru.jenisKelamin}</h1>
+                    <h1 className="px-2 text-lg">
+                      Jenis Kelamin : {props.dataGuru.jenisKelamin}
+                    </h1>
                   </div>
                   <div className="flex items-center mt-4 text-gray-700">
-                    <h1 className="px-2 text-lg">Saldo : {props.dataGuru.sisaSaldo}</h1>
+                    <h1 className="px-2 text-lg">
+                      Saldo : {props.dataGuru.sisaSaldo}
+                    </h1>
                   </div>
                 </div>
                 {/* <Link
@@ -94,11 +103,17 @@ const Rincian = (props) => {
               Cek Saldo
             </Link> */}
                 <div>
-                  <button ref={addButton} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="large-modal">
+                  <button
+                    ref={addButton}
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    type="button"
+                    data-modal-toggle="large-modal"
+                  >
                     Cek saldo
                   </button>
                   &nbsp;
-                  <Link ref={addButton}
+                  <Link
+                    ref={addButton}
                     href="/"
                     type="button"
                     className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
@@ -106,9 +121,11 @@ const Rincian = (props) => {
                     Kembali
                   </Link>
                   &nbsp;
-                  <button ref={addButton}
+                  <button
+                    ref={addButton}
                     // href="/print"
-                    type="button" onClick={printDiv}
+                    type="button"
+                    onClick={printDiv}
                     className="text-white bg-lime-700 hover:bg-lime-800 focus:ring-4 focus:outline-none focus:ring-lime-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-lime-600 dark:hover:bg-lime-700 dark:focus:ring-lime-800"
                   >
                     Print
@@ -123,23 +140,31 @@ const Rincian = (props) => {
           <div>
             <form onSubmit={handleSubmitBulan}>
               <label>Menurut bulan</label>
-              <input type="month" id="month" onChange={(bulan) => setBulan(bulan.target.value)} />
-              <button type="submit" className="bg-sky-500 text-white p-2 mt-3 rounded-md">Cek saldo</button>
+              <input
+                type="month"
+                id="month"
+                onChange={(bulan) => setBulan(bulan.target.value)}
+              />
+              <button
+                type="submit"
+                className="bg-sky-500 text-white p-2 mt-3 rounded-md"
+              >
+                Cek saldo
+              </button>
             </form>
           </div>
           <div>
             <label>Menurut Tahun</label>
             {/* <Input type="year" className="w-full" />
-                   */}
-            <Link className="bg-sky-500 text-white p-2 mt-3 rounded-md">Cek saldo</Link>
+             */}
+            <Link className="bg-sky-500 text-white p-2 mt-3 rounded-md">
+              Cek saldo
+            </Link>
           </div>
         </div>
-
       </div>
 
       {/* <iframe id="print-frame" style={{display: 'none'}}></iframe> */}
-
-
     </>
   );
 };
