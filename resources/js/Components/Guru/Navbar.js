0a7b0cb3caed17bd80ad4c73b1f40a6c1f7bdff9
Dzaky Fadli <zaaaafl654@gmail.com>
2022-12-29 13:50:57 +0700

    simple search

--- resources/js/Components/Guru/Navbar.js
+++ resources/js/Components/Guru/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
             <Link
               href=""
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-rose-600 hover:bg-rose-700 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-md px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800"
             >
               Logout
             </Link>
@@ -42,7 +42,6 @@ export default function Navbar() {
               </svg>
             </button>
           </div>
-
         </div>
       </nav>
     </>
--- resources/js/Pages/Home/Utama.js
+++ resources/js/Pages/Home/Utama.js
@@ -3,10 +3,18 @@ import Authenticated from "@/Layouts/Authenticated";
 import { Inertia } from "@inertiajs/inertia";
 import { Link } from "@inertiajs/inertia-react";
 import axios from "axios";
-import React from "react";
-// import FileSaver from "file-saver";
+import React, { useState } from "react";
 
 const Utama = (props) => {
+  const [query, setQuery] = useState("");
+  const keys = ["nama", "alamat"];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+
   function formatRupiah(angka) {
     return angka.toLocaleString("id-ID", {
       style: "currency",
@@ -28,15 +36,6 @@ const Utama = (props) => {
         link.click();
       });
   }
-
-  // Output: "Rp100.000"
-  // async function downloadExcel() {
-  //   const response = await Inertia.get("/cetak-data", {
-  //     responseType: "",
-  //   });
-  //   FileSaver.saveAs(new Blob([response.dataGuru]), "dataguru.xlsx");
-  // }
-
   //untuk print data
   function handlePrintOtherPage() {
     const newWindow = window.open("/print-data");
@@ -55,6 +54,40 @@ const Utama = (props) => {
         <div className="text-center font-bold mx-auto my-4 text-2xl">
           <h1>Data Guru</h1>
         </div>
+        {/* start search field */}
+        <div className="mt-1 mb-7 mx-auto max-w-xl">
+          <div className="flex items-center">
+            <label htmlFor="search-guru" className="sr-only">
+              Cari...
+            </label>
+            <div className="relative w-full">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="text"
+                id="search-guru"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Cari..."
+                onChange={(e) => setQuery(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </div>
+        {/* end search field */}
         <div className="flex justify-end items-end m-4">
           <Link
             href="/tambahdata"
@@ -122,8 +155,8 @@ const Utama = (props) => {
               </tr>
             </thead>
             <tbody>
-              {props.dataGuru ? (
-                props.dataGuru.map((data, i) => {
+              {search(props.dataGuru) ? (
+                search(props.dataGuru).map((data, i) => {
                   return (
                     <tr
                       key={i}
