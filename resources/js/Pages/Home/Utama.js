2fa68a4b8b07fa1f7150b117b9a314bfba3cbe3c
zionexeonch <zionexeon@gmail.com>
2022-12-29 13:37:12 +0700

    fasfasd

--- resources/js/Pages/Home/Utama.js
+++ resources/js/Pages/Home/Utama.js
@@ -14,6 +14,20 @@ const Utama = (props) => {
     });
   }
 
+  function exportToExcel() {
+    axios
+      .get("/cetak-data", {
+        responseType: "blob",
+      })
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "dataguru.xlsx");
+        document.body.appendChild(link);
+        link.click();
+      });
+  }
 
   // Output: "Rp100.000"
   // async function downloadExcel() {
@@ -21,7 +35,7 @@ const Utama = (props) => {
   //     responseType: "",
   //   });
   //   FileSaver.saveAs(new Blob([response.dataGuru]), "dataguru.xlsx");
-  // }  
+  // }
 
   function handlePrintOtherPage() {
     const newWindow = window.open("/print-data");
@@ -34,14 +48,11 @@ const Utama = (props) => {
     };
   }
 
-
-
   return (
     <>
       <div className="container mx-auto my-4">
         <div className="text-center font-bold mx-auto my-4 text-2xl">
           <h1>Data Guru</h1>
-
         </div>
         <div className="flex justify-end items-end m-4">
           <Link
@@ -52,22 +63,32 @@ const Utama = (props) => {
             Tambah Guru
           </Link>
           &nbsp;
-          <Link
-            href="/cetak-data"
+          <button
+            onClick={exportToExcel}
             type="button"
             className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-md text-xs md:text-sm p-1 md:p-2 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
           >
             Export Excel
-          </Link>
+          </button>
           &nbsp;
-
           <button
             onClick={handlePrintOtherPage}
             type="button"
             className="flex text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-md text-xs md:text-sm p-1 md:p-2 text-center mr-3 md:mr-0 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-500"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z"
+              />
             </svg>
             Cetak Data Guru
           </button>
@@ -161,8 +182,7 @@ const Utama = (props) => {
             </tbody>
           </table>
         </div>
-
-      </div>      
+      </div>
     </>
   );
 };
